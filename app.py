264c64627584ba401c6f33cb3f54fb3649e4f4f0
trippleflipp <trippleflippbeats@gmail.com>
2025-04-09 21:10:07 +0700

    if

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True) 
-    #app.run(host=0.0.0.0, port=2634)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=2634)
