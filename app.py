86b21974bb7fe1d2791c07a403e537cc26debc9b
trippleflipp <trippleflippbeats@gmail.com>
2025-04-09 23:10:22 +0700

    goaaal

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 from app import create_app, db
 import ssl
-try:
-    from waitress import serve
-    import waitress  # Import waitress here as well
-except ImportError as e:
-    print(f"Error importing waitress: {e}")
-    print("Please ensure waitress is installed.  Try: pip install waitress")
-    exit(1)
-
 
 app = create_app()
 
@@ -21,6 +13,4 @@ if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain(cert_file, key_file)
 
-    # Serve the app with SSL
-    serve(app, host="0.0.0.0", port=443, ssl=context)
-    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=2365, ssl_context=(cert_file, key_file))
