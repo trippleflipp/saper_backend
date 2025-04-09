2a053f59b62b21269be63e48185c48875cfddd64
trippleflipp <trippleflippbeats@gmail.com>
2025-04-09 22:17:27 +0700

    Added ssl context

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from app import create_app, db
 from waitress import serve
+import ssl
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+
+    cert_file = "ssl_cert.pem"
+    key_file = "ssl_key.pem"
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(cert_file, key_file)
+
+    # Serve the app with SSL
+    serve(app, host="0.0.0.0", port=443, ssl_context=context)
     #app.run(debug=True)
-    serve(app, host="0.0.0.0", port=443, ssl_cert="ssl_cert.pem", ssl_key="ssl_key.pem")
